4828cddd191ebba17cb9b5f56edae59501e1b65b
fatou CAMARA <fatou.thithi.camara@gmail.com>
2015-08-18 14:46:41 +0200

    Etudiant Mapper

--- gen-service/src/main/java/gn/scs/gen/transformer/EtudiantMapper.java
+++ gen-service/src/main/java/gn/scs/gen/transformer/EtudiantMapper.java
@@ -16,15 +16,59 @@ import gn.scs.gen.model.Etudiant;
 public class EtudiantMapper implements Mapper<Etudiant,EtudiantDTO>{
 
 	@Override
-	public EtudiantDTO map(Etudiant type) {
-		// TODO Auto-generated method stub
-		return null;
+	public EtudiantDTO map(Etudiant etudiant) {
+		EtudiantDTO etudiantDTO=null;
+
+		if(etudiant!=null){
+			etudiantDTO=new EtudiantDTO();
+
+			if(etudiant.getIdEtudiant()!=null){
+				etudiantDTO.setIdEtudiant(etudiant.getIdEtudiant());
+			}
+
+			etudiantDTO.setEmail(etudiant.getEmail());
+
+			etudiantDTO.setMatricule(etudiant.getMatricule());
+			etudiantDTO.setNom(etudiant.getNom());
+			etudiantDTO.setPrenom(etudiant.getPrenom());
+			etudiantDTO.setSexe(etudiant.getSexe());
+			etudiantDTO.setSituationMatrimoniale(etudiant.getSituationMatrimoniale());
+			etudiantDTO.setNationalite(etudiant.getNationalite());
+			etudiantDTO.setAdresse(etudiant.getAdresse());
+			etudiantDTO.setTelephone(etudiant.getTelephone());
+			etudiantDTO.setTelephoneTuteur(etudiant.getTelephoneTuteur());
+			etudiantDTO.setTuteur(etudiant.getTuteur());
+		}
+
+		return etudiantDTO;
 	}
 
 	@Override
-	public Etudiant unmap(EtudiantDTO result) {
-		// TODO Auto-generated method stub
-		return null;
+	public Etudiant unmap(EtudiantDTO etudiantDTO) {
+		Etudiant etudiant=null;
+
+		if(etudiantDTO!=null){
+			etudiant=new Etudiant();
+
+			if(etudiantDTO.getIdEtudiant()!=null){
+				etudiant.setIdEtudiant(etudiantDTO.getIdEtudiant());
+			}
+
+			etudiant.setEmail(etudiantDTO.getEmail());
+
+			etudiant.setMatricule(etudiantDTO.getMatricule());
+			etudiant.setNom(etudiantDTO.getNom());
+			etudiant.setPrenom(etudiantDTO.getPrenom());
+			etudiant.setSexe(etudiantDTO.getSexe());
+			etudiant.setSituationMatrimoniale(etudiantDTO.getSituationMatrimoniale());
+			etudiant.setNationalite(etudiantDTO.getNationalite());
+			etudiant.setAdresse(etudiantDTO.getAdresse());
+			etudiant.setTelephone(etudiantDTO.getTelephone());
+			etudiant.setTelephoneTuteur(etudiantDTO.getTelephoneTuteur());
+			etudiant.setTuteur(etudiantDTO.getTuteur());
+		}
+
+		return etudiant;
 	}
 
 
@@ -32,5 +76,5 @@ public class EtudiantMapper implements Mapper<Etudiant,EtudiantDTO>{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
